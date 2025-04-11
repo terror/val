513acc6c1e226661eb11a3472bf02159d9d9f326
Liam <liam@scalzulli.com>
2025-04-11 14:25:23 -0400

    Parse function calls

--- src/parser.rs
+++ src/parser.rs
@@ -2,18 +2,31 @@ use super::*;
 
 pub(crate) fn parser<'a>()
 -> impl Parser<'a, &'a str, Spanned<Ast<'a>>, extra::Err<Rich<'a, char>>> {
-  let ident = text::ident().padded();
+  let identifier = text::ident().padded();
 
-  let expr = recursive(|_| {
+  recursive(|expr| {
     let number = text::int(10)
       .map(|s: &str| Ast::Number(s.parse().unwrap()))
       .map_with(|ast, e| (ast, e.span()));
 
-    let atom = number.or(
-      ident
-        .map(Ast::Identifier)
-        .map_with(|ast, e| (ast, e.span())),
-    );
+    let call = identifier
+      .then(
+        expr
+          .clone()
+          .delimited_by(just('('), just(')'))
+          .separated_by(just(','))
+          .allow_trailing()
+          .collect::<Vec<_>>()
+          .delimited_by(just('('), just(')')),
+      )
+      .map(|(f, args)| Ast::Call(f, args))
+      .map_with(|ast, e| (ast, e.span()));
+
+    let identifier = identifier
+      .map(Ast::Identifier)
+      .map_with(|ast, e| (ast, e.span()));
+
+    let atom = number.or(call).or(identifier);
 
     let op = |c| just(c).padded();
 
@@ -23,7 +36,5 @@ pub(crate) fn parser<'a>()
     });
 
     unary
-  });
-
-  expr
+  })
 }
