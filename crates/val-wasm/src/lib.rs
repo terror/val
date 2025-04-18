4521c065e1572d1180a225c78109d75a1439341f
Liam <liam@scalzulli.com>
2025-04-18 14:30:01 -0400

    Export `eval` function to wasm

--- crates/val-wasm/src/lib.rs
+++ crates/val-wasm/src/lib.rs
@@ -1,10 +1,15 @@
 use {
-  crate::{ast_node::AstNode, range::Range},
-  val::{Expression, Program, Span, Statement},
+  crate::{
+    ast_node::AstNode,
+    error::{ErrorKind, ValError},
+    range::Range,
+  },
+  val::{Environment, Evaluator, Expression, Program, Span, Statement},
   wasm_bindgen::prelude::*,
 };
 
 mod ast_node;
+mod error;
 mod range;
 
 #[wasm_bindgen(start)]
@@ -12,20 +17,44 @@ fn start() {
   console_error_panic_hook::set_once();
 }
 
-#[wasm_bindgen(getter_with_clone)]
-pub struct ParseError {
-  pub message: String,
-  pub range: Range,
-}
-
 #[wasm_bindgen]
-pub fn parse(input: &str) -> Result<AstNode, Vec<ParseError>> {
+pub fn parse(input: &str) -> Result<AstNode, Vec<ValError>> {
   match val::parse(input) {
     Ok((ast, span)) => Ok(AstNode::from((&ast, &span))),
     Err(errors) => Err(
       errors
         .into_iter()
-        .map(|error| ParseError {
+        .map(|error| ValError {
+          kind: ErrorKind::Parser,
+          message: error.message,
+          range: Range::from(error.span),
+        })
+        .collect(),
+    ),
+  }
+}
+
+#[wasm_bindgen]
+pub fn eval(input: &str) -> Result<String, Vec<ValError>> {
+  match val::parse(input) {
+    Ok(ast) => {
+      let mut evaluator =
+        Evaluator::from(Environment::new(val::Config::default()));
+
+      match evaluator.eval(&ast) {
+        Ok(value) => Ok(value.to_string()),
+        Err(error) => Err(vec![ValError {
+          kind: ErrorKind::Evaluator,
+          message: error.message,
+          range: Range::from(error.span),
+        }]),
+      }
+    }
+    Err(errors) => Err(
+      errors
+        .into_iter()
+        .map(|error| ValError {
+          kind: ErrorKind::Parser,
           message: error.message,
           range: Range::from(error.span),
         })
--- www/src/components/editor.tsx
+++ www/src/components/editor.tsx
@@ -9,11 +9,11 @@ import {
 } from '@codemirror/language';
 import { Diagnostic, lintGutter, linter } from '@codemirror/lint';
 import CodeMirror, { EditorState, EditorView } from '@uiw/react-codemirror';
-import { ParseError } from 'packages/val-wasm/val';
 import { forwardRef, useCallback, useImperativeHandle, useRef } from 'react';
+import { ValError } from 'val-wasm';
 
 interface EditorProps {
-  errors: ParseError[];
+  errors: ValError[];
   onChange?: (value: string, viewUpdate: any) => void;
   value: string;
 }
@@ -54,9 +54,6 @@ export const Editor = forwardRef<EditorRef, EditorProps>(
           },
           '.cm-content': {
             padding: '10px 0',
-          },
-          '.cm-line': {
-            padding: '0 10px',
           },
           '.cm-gutters': {
             backgroundColor: 'transparent',
@@ -103,30 +100,27 @@ export const Editor = forwardRef<EditorRef, EditorProps>(
     const diagnostics = () =>
       useCallback(
         (_view: EditorView): Diagnostic[] => {
-          return (
-            errors &&
-            errors.map((error) => {
-              try {
-                return {
-                  from: error.range.start,
-                  to: error.range.end,
-                  severity: 'error',
-                  message: error.message,
-                  source: 'PARSER',
-                };
-              } catch (e) {
-                console.warn('Failed to create diagnostic:', e, error);
+          return errors.map((error) => {
+            try {
+              return {
+                from: error.range.start,
+                to: error.range.end,
+                severity: 'error',
+                message: error.message,
+                source: error.kind.toString(),
+              };
+            } catch (e) {
+              console.warn('Failed to create diagnostic:', e, error);
 
-                return {
-                  from: 0,
-                  to: 0,
-                  severity: 'error',
-                  message: error.message,
-                  source: 'PARSER',
-                };
-              }
-            })
-          );
+              return {
+                from: 0,
+                to: 0,
+                severity: 'error',
+                message: error.message,
+                source: error.kind.toString(),
+              };
+            }
+          });
         },
         [errors]
       );
@@ -136,6 +130,8 @@ export const Editor = forwardRef<EditorRef, EditorProps>(
         value={value}
         theme={createEditorTheme()}
         basicSetup={{
+          foldGutter: false,
+          highlightActiveLineGutter: false,
           lineNumbers: settings.lineNumbers,
         }}
         height='100%'
