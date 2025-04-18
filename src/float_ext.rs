5b710d8a40066e2dcca5c6aa25af2336fb38d0df
Liam <liam@scalzulli.com>
2025-04-18 18:53:39 -0400

    Add additional float display tests

--- src/float_ext.rs
+++ src/float_ext.rs
@@ -7,72 +7,69 @@ pub trait FloatExt {
 
 impl FloatExt for Float {
   fn display(&self) -> String {
-    if self.is_nan() {
-      return "nan".into();
+    match () {
+      _ if self.is_nan() => "nan".into(),
+      _ if self.is_inf_pos() => "inf".into(),
+      _ if self.is_inf_neg() => "-inf".into(),
+      _ if self.is_zero() => "0".into(),
+      _ => {
+        let formatted = self
+          .format(
+            Radix::Dec,
+            astro_float::RoundingMode::None,
+            &mut Consts::new().unwrap(),
+          )
+          .unwrap();
+
+        if !formatted.contains('e') {
+          return formatted;
+        }
+
+        let (mantissa, exponent) = {
+          let mut parts = formatted.split('e');
+          (parts.next().unwrap(), parts.next().unwrap())
+        };
+
+        let exponent = exponent.parse::<i32>().unwrap();
+
+        let (sign, mantissa) =
+          mantissa.split_at(if mantissa.starts_with('-') { 1 } else { 0 });
+
+        let digits = mantissa.replace('.', "");
+
+        let length =
+          mantissa.find('.').unwrap_or(mantissa.len()) as i32 + exponent;
+
+        let result = match length {
+          length if length <= 0 => {
+            format!("{}0.{}{}", sign, "0".repeat((-length) as usize), digits)
+          }
+          length if (length as usize) >= digits.len() => {
+            format!(
+              "{}{}{}",
+              sign,
+              digits,
+              "0".repeat(length as usize - digits.len())
+            )
+          }
+          _ => {
+            let (left, right) = digits.split_at(length as usize);
+            format!("{}{}.{}", sign, left, right)
+          }
+        };
+
+        if result.find('.').is_some() {
+          return result
+            .trim_end_matches('0')
+            .trim_end_matches('.')
+            .to_string();
+        }
+
+        result
+      }
     }
-
-    if self.is_inf_pos() {
-      return "inf".into();
-    }
-
-    if self.is_inf_neg() {
-      return "-inf".into();
-    }
-
-    if self.is_zero() {
-      return "0".into();
-    }
-
-    let formatted = self
-      .format(
-        Radix::Dec,
-        astro_float::RoundingMode::None,
-        &mut Consts::new().unwrap(),
-      )
-      .unwrap();
-
-    if !formatted.contains('e') {
-      return formatted;
-    }
-
-    let (mant, exp) = {
-      let mut parts = formatted.split('e');
-      (parts.next().unwrap(), parts.next().unwrap())
-    };
-
-    let exp = exp.parse::<i32>().unwrap();
-
-    let (sign, mant) = mant.split_at(if mant.starts_with('-') { 1 } else { 0 });
-
-    let digits = mant.replace('.', "");
-
-    let length = mant.find('.').unwrap_or(mant.len()) as i32 + exp;
-
-    let result = if length <= 0 {
-      format!("{}0.{}{}", sign, "0".repeat((-length) as usize), digits)
-    } else if length as usize >= digits.len() {
-      format!(
-        "{}{}{}",
-        sign,
-        digits,
-        "0".repeat(length as usize - digits.len())
-      )
-    } else {
-      let (left, right) = digits.split_at(length as usize);
-      format!("{}{}.{}", sign, left, right)
-    };
-
-    if result.find('.').is_some() {
-      return result
-        .trim_end_matches('0')
-        .trim_end_matches('.')
-        .to_string();
-    }
-
-    result
   }
 
-  // https://github.com/stencillogic/astro-float/issues/11
   fn to_f64(&self, rounding_mode: astro_float::RoundingMode) -> Option<f64> {
     let mut big_float = self.clone();
 
@@ -140,6 +137,16 @@ impl FloatExt for Float {
 mod tests {
   use super::*;
 
+  fn float_from_str(s: &str) -> Float {
+    Float::parse(
+      s,
+      Radix::Dec,
+      128,
+      astro_float::RoundingMode::FromZero,
+      &mut Consts::new().unwrap(),
+    )
+  }
+
   #[test]
   fn specials() {
     assert_eq!(format!("{}", Float::from(0).display()), "0");
@@ -152,5 +159,56 @@ mod tests {
     );
 
     assert_eq!(format!("{}", Float::nan(None).display()), "nan");
+  }
+
+  #[test]
+  fn integers() {
+    assert_eq!(Float::from(1).display(), "1");
+    assert_eq!(Float::from(-1).display(), "-1");
+    assert_eq!(Float::from(123456789).display(), "123456789");
+    assert_eq!(Float::from(-123456789).display(), "-123456789");
+  }
+
+  #[test]
+  fn trailing_zeros() {
+    assert_eq!(float_from_str("1.2300e2").display(), "123");
+  }
+
+  #[test]
+  fn scientific_notation_positive_exponent() {
+    assert_eq!(float_from_str("1.23e2").display(), "123");
+    assert_eq!(float_from_str("1.23e3").display(), "1230");
+  }
+
+  #[test]
+  fn scientific_notation_negative_exponent() {
+    assert_eq!(float_from_str("1.23e-5").display(), "0.0000123");
+    assert_eq!(float_from_str("-1.23e-2").display(), "-0.0123");
+    assert_eq!(float_from_str("-1.23e-5").display(), "-0.0000123");
+  }
+
+  #[test]
+  fn large_numbers() {
+    assert_eq!(float_from_str("1e15").display(), "1000000000000000");
+    assert_eq!(float_from_str("-1e15").display(), "-1000000000000000");
+    assert_eq!(float_from_str("1.23e15").display(), "1230000000000000");
+  }
+
+  #[test]
+  fn convert_to_double_precision() {
+    assert_eq!(
+      Float::from(0.0).to_f64(astro_float::RoundingMode::ToEven),
+      Some(0.0)
+    );
+
+    assert_eq!(
+      Float::from(1.0).to_f64(astro_float::RoundingMode::ToEven),
+      Some(1.0)
+    );
+
+    assert_eq!(
+      Float::from(-1.0).to_f64(astro_float::RoundingMode::ToEven),
+      Some(-1.0)
+    );
   }
 }
