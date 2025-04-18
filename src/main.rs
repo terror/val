aca4ccb8e22712332af19058bc060d4b854ec56a
liam <liam@scalzulli.com>
2025-04-18 01:47:23 -0400

    Allow users to configure the stack size (#20)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use {
   clap::Parser,
   rustyline::error::ReadlineError,
-  std::{backtrace::BacktraceStatus, process},
+  std::{backtrace::BacktraceStatus, process, thread},
   val::arguments::Arguments,
 };
 
 fn main() {
-  if let Err(error) = Arguments::parse().run() {
+  let arguments = Arguments::parse();
+
+  let stack_size = arguments.stack_size * 1024 * 1024;
+
+  let result = thread::Builder::new()
+    .stack_size(stack_size)
+    .spawn(move || arguments.run())
+    .unwrap()
+    .join();
+
+  if let Err(error) =
+    result.unwrap_or_else(|_| Err(anyhow::anyhow!("Thread panicked")))
+  {
     if let Some(error) = error.downcast_ref::<ReadlineError>() {
       if matches!(error, ReadlineError::Eof | ReadlineError::Interrupted) {
         return;
