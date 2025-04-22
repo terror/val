2fbd9f36ffaad347e88cd5ab5fb5e8dc24a970a0
liam <liam@scalzulli.com>
2025-04-21 23:57:38 -0400

    Add benchmark for precise value increments (#47)

--- benches/main.rs
+++ benches/main.rs
@@ -3,13 +3,12 @@ use {
   val::{Environment, Evaluator},
 };
 
-fn bench_recursive_factorial(criterion: &mut Criterion) {
-  let mut group = criterion.benchmark_group("recursive_factorial");
+fn bench_increment_value(criterion: &mut Criterion) {
+  let mut group = criterion.benchmark_group("increment_value");
 
   for &number in &[10_u32, 50, 100, 500] {
-    let program = format!(
-      "fn f(x) {{ if ( x <= 1) {{ return 1 }} else {{ return x * f(x - 1) }} }} f({number})"
-    );
+    let program =
+      format!("a = 0.001; while (a < {number}) {{ a = a + 0.001 }}; a");
 
     let ast = val::parse(&program).unwrap();
 
@@ -79,6 +78,31 @@ fn bench_prime_count(criterion: &mut Criterion) {
   group.finish();
 }
 
-criterion_group!(benches, bench_recursive_factorial, bench_prime_count);
+fn bench_recursive_factorial(criterion: &mut Criterion) {
+  let mut group = criterion.benchmark_group("recursive_factorial");
+
+  for &number in &[10_u32, 50, 100, 500] {
+    let program = format!(
+      "fn f(x) {{ if ( x <= 1) {{ return 1 }} else {{ return x * f(x - 1) }} }} f({number})"
+    );
+
+    let ast = val::parse(&program).unwrap();
+
+    group.bench_function(format!("n = {number}"), |bencher| {
+      bencher.iter(|| {
+        black_box(Evaluator::from(Environment::default()).eval(&ast)).unwrap();
+      })
+    });
+  }
+
+  group.finish();
+}
+
+criterion_group!(
+  benches,
+  bench_increment_value,
+  bench_prime_count,
+  bench_recursive_factorial
+);
 
 criterion_main!(benches);
