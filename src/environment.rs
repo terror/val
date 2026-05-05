b116f4f9042139b4a4d9e50ccabd5922750200de
liam <liam@scalzulli.com>
2026-05-04 20:46:06 -0400

    Unify builtin and user-defined function values (#66)

--- src/environment.rs
+++ src/environment.rs
@@ -3,28 +3,37 @@ use super::*;
 #[derive(Clone, Debug, Default)]
 pub struct Environment<'src> {
   pub config: Config,
-  pub functions: HashMap<&'src str, Function<'src>>,
   pub parent: Option<Box<Environment<'src>>>,
-  pub variables: HashMap<&'src str, Value<'src>>,
+  symbols: HashMap<&'src str, Symbol<'src>>,
+}
+
+#[derive(Clone, Debug, Default)]
+struct Symbol<'src> {
+  function: Option<Function<'src>>,
+  value: Option<Value<'src>>,
 }
 
 impl<'src> Environment<'src> {
   pub fn new(config: Config) -> Self {
     let mut environment = Self {
       config: config.clone(),
-      functions: HashMap::new(),
       parent: None,
-      variables: HashMap::new(),
+      symbols: HashMap::new(),
     };
 
     for builtin in BUILTINS {
       match builtin {
         Builtin::Constant { value, .. } => {
-          environment.add_variable(builtin.name(), value(config.clone()));
+          environment.add_symbol(builtin.name(), value(config.clone()));
         }
         Builtin::Function { function, .. } => {
-          environment
-            .add_function(builtin.name(), Function::Builtin(*function));
+          environment.add_function(
+            builtin.name(),
+            Function::Builtin {
+              function: *function,
+              name: builtin.name(),
+            },
+          );
         }
       }
     }
@@ -32,12 +41,12 @@ impl<'src> Environment<'src> {
     environment
   }
 
-  pub fn add_function(&mut self, name: &'src str, function: Function<'src>) {
-    self.functions.insert(name, function);
+  pub fn add_symbol(&mut self, name: &'src str, value: Value<'src>) {
+    self.symbols.entry(name).or_default().value = Some(value);
   }
 
-  pub fn add_variable(&mut self, name: &'src str, value: Value<'src>) {
-    self.variables.insert(name, value);
+  pub fn add_function(&mut self, name: &'src str, function: Function<'src>) {
+    self.symbols.entry(name).or_default().function = Some(function);
   }
 
   pub fn call_function(
@@ -46,75 +55,10 @@ impl<'src> Environment<'src> {
     arguments: Vec<Value<'src>>,
     span: Span,
   ) -> Result<Value<'src>, Error> {
-    if let Some(function) = self.functions.get(name) {
-      match function {
-        Function::Builtin(function) => function(BuiltinFunctionPayload {
-          arguments,
-          config: self.config.clone(),
-          span,
-        }),
-        Function::UserDefined(Value::Function(
-          name,
-          parameters,
-          body,
-          environment,
-        )) => {
-          if parameters.len() != arguments.len() {
-            return Err(Error::new(
-              span,
-              format!(
-                "Function `{}` expects {} arguments, got {}",
-                name,
-                parameters.len(),
-                arguments.len()
-              ),
-            ));
-          }
-
-          let mut call_environment =
-            Environment::with_parent(environment.clone());
-
-          call_environment.add_function(name, function.clone());
-
-          for (parameter, argument) in parameters.iter().zip(arguments.iter()) {
-            let parameter_name = *parameter;
-
-            match argument {
-              Value::Function(_, _, _, _) => {
-                call_environment.add_function(
-                  parameter_name,
-                  Function::UserDefined(argument.clone()),
-                );
-              }
-              Value::BuiltinFunction(_, builtin) => {
-                call_environment
-                  .add_function(parameter_name, Function::Builtin(*builtin));
-              }
-              _ => {
-                call_environment.add_variable(parameter_name, argument.clone());
-              }
-            }
-          }
-
-          let mut evaluator = Evaluator::from(call_environment);
-          evaluator.inside_function = true;
-
-          if body.is_empty() {
-            return Ok(Value::Null);
-          }
-
-          for statement in body.iter() {
-            let result = evaluator.eval_statement(statement)?;
-
-            if result.is_return() {
-              return Ok(result.unwrap());
-            }
-          }
-
-          Ok(Value::Null)
-        }
-        _ => Err(Error::new(span, format!("`{}` is not a function", name))),
-      }
+    if let Some(function) = self.resolve_function(name) {
+      function.call(arguments, self.config.clone(), span)
+    } else if self.resolve_symbol(name).is_some() {
+      Err(Error::new(span, format!("`{}` is not a function", name)))
     } else {
       Err(Error::new(
         span,
@@ -123,16 +67,30 @@ impl<'src> Environment<'src> {
     }
   }
 
+  fn resolve_function(&self, name: &str) -> Option<Function<'src>> {
+    if let Some(symbol) = self.symbols.get(name) {
+      if let Some(function) = &symbol.function {
+        Some(function.clone())
+      } else if let Some(Value::Function(function)) = &symbol.value {
+        Some(function.clone())
+      } else if let Some(parent) = &self.parent {
+        parent.resolve_function(name)
+      } else {
+        None
+      }
+    } else if let Some(parent) = &self.parent {
+      parent.resolve_function(name)
+    } else {
+      None
+    }
+  }
+
   pub fn resolve_symbol(&self, symbol: &str) -> Option<Value<'src>> {
-    if let Some(value) = self.variables.get(symbol) {
-      Some(value.clone())
-    } else if let Some((name, function)) = self.functions.get_key_value(symbol)
-    {
-      match function {
-        Function::UserDefined(value) => Some(value.clone()),
-        Function::Builtin(builtin) => {
-          Some(Value::BuiltinFunction(name, *builtin))
-        }
+    if let Some(symbol) = self.symbols.get(symbol) {
+      if let Some(value) = &symbol.value {
+        Some(value.clone())
+      } else {
+        symbol.function.clone().map(Value::Function)
       }
     } else if let Some(parent) = &self.parent {
       parent.resolve_symbol(symbol)
@@ -144,9 +102,8 @@ impl<'src> Environment<'src> {
   pub fn with_parent(parent: Environment<'src>) -> Self {
     Self {
       config: parent.config.clone(),
-      functions: parent.functions.clone(),
       parent: Some(Box::new(parent)),
-      variables: HashMap::new(),
+      symbols: HashMap::new(),
     }
   }
 }
--- src/function.rs
+++ src/function.rs
@@ -1,7 +1,82 @@
 use super::*;
 
 #[derive(Clone, Debug)]
-pub enum Function<'a> {
-  Builtin(BuiltinFunction),
-  UserDefined(Value<'a>),
+pub enum Function<'src> {
+  Builtin {
+    function: BuiltinFunction,
+    name: &'src str,
+  },
+  UserDefined {
+    body: Vec<Spanned<Statement<'src>>>,
+    environment: Environment<'src>,
+    name: &'src str,
+    parameters: Vec<&'src str>,
+  },
+}
+
+impl<'src> Function<'src> {
+  pub fn call(
+    &self,
+    arguments: Vec<Value<'src>>,
+    config: Config,
+    span: Span,
+  ) -> Result<Value<'src>, Error> {
+    match self {
+      Self::Builtin { function, .. } => function(BuiltinFunctionPayload {
+        arguments,
+        config,
+        span,
+      }),
+      Self::UserDefined {
+        body,
+        environment,
+        name,
+        parameters,
+      } => {
+        if parameters.len() != arguments.len() {
+          return Err(Error::new(
+            span,
+            format!(
+              "Function `{}` expects {} arguments, got {}",
+              name,
+              parameters.len(),
+              arguments.len()
+            ),
+          ));
+        }
+
+        let mut call_environment =
+          Environment::with_parent(environment.clone());
+
+        call_environment.add_function(name, self.clone());
+
+        for (parameter, argument) in parameters.iter().zip(arguments.iter()) {
+          call_environment.add_symbol(parameter, argument.clone());
+        }
+
+        let mut evaluator = Evaluator::from(call_environment);
+        evaluator.inside_function = true;
+
+        if body.is_empty() {
+          return Ok(Value::Null);
+        }
+
+        for statement in body.iter() {
+          let result = evaluator.eval_statement(statement)?;
+
+          if result.is_return() {
+            return Ok(result.unwrap());
+          }
+        }
+
+        Ok(Value::Null)
+      }
+    }
+  }
+
+  pub fn name(&self) -> &'src str {
+    match self {
+      Self::Builtin { name, .. } | Self::UserDefined { name, .. } => name,
+    }
+  }
 }
